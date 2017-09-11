1800838fbafdf0b25eff8cff1f3fed28b1b04b80
Paul Merrill <paul.merrill@beaufortfairmont.com>
2017-09-11 13:36:24 -0400

    First steps, add order test and order class.

---
+++ src/main/java/beaufort/retailordering/Order.java
@@ -0,0 +1,9 @@
+package beaufort.retailordering;
+
+public class Order {
+
+	public Object purchase(String sku) {
+		throw new UnsupportedOperationException("This method not yet implemented");
+	}
+
+}
--- src/test/java/beaufort/retailordering/OrderTests.java
+++ src/test/java/beaufort/retailordering/OrderTests.java
@@ -5,15 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class OrderTests {
+	Order order;
 
 	@Before
 	public void setUp() throws Exception {
+		order = new Order();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void purchaseTest() {
+		assertNotNull(order.purchase("12345678"));
+		
 	}
 
 }
